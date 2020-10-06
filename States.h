8869ba76590f7c52d145e2ea5f2008cda670fdfe
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-07 01:07:51 +0200

    Removed old states and added trivial examples

--- States.h
+++ States.h
@@ -1,8 +1,15 @@
 #pragma once
 
+/*/
+Add desired states in this class, used later
+when insatntiating a StateMachine to bind
+with a function / method.
+*/
+
 enum class State
 {
-	LED_CYCLE,
-	LED_RAINBOW,
-	WAITING
+	IDLE,	// Default main state, editing requires modification of StateMachine.h row 80 col 57
+	READ_PROXIMITY_SENSOR,
+	STOP_MOTORS,
+	CHECK_KEY_PRESS
 };
