3d41368f243accded3364b109fc7bdeca857ffb4
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-07 01:10:16 +0200

    Supporting transitional states, this method returns the method that is next in line for a given current state. This way, a state can "chain" a maximum of (1) secondary state to automatically be selected when it calls the release() method.

--- StateMachine.cpp
+++ StateMachine.cpp
@@ -16,7 +16,26 @@ fp_t StateMachine::getState(State state)
 {
 	fp_t func;
 
-	for (int i = 0; i < lastAddedState; i++)
+
+const State StateMachine::getTransitionalStateForState(State state)
+/*
+* Get the next state in line, as per defined
+* by the given state the machine is currently
+* in at the time of calling. The default is 
+* the mainState state as a failsafe fallback,
+* if loop exhausts without finding a matching
+* transition for the current state. 
+*/
+{
+	for (int i = 0; i < this->stateCount; i++)
+	{
+		if (this->states[i] == state)
+		{
+			return this->transitionalStates[i];
+		}
+	}
+	return this->mainState;
+}
 	{
 		if (states[i] == state)
 		{
